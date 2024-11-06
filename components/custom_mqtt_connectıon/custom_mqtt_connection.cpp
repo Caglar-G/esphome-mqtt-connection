3f656517e85608eb1d8604f30ce3c2515693a76d
caglar <caglarglck@yandex.com>
2024-11-06 04:37:24 +0300

    fix add mqtt

--- components/custom_mqtt_connectıon/custom_mqtt_connection.cpp
+++ components/custom_mqtt_connectıon/custom_mqtt_connection.cpp
@@ -31,10 +31,10 @@ namespace custom_mqtt_connection {
             // Switch durumu değiştiğinde bu kod bloğu çalışır
             if (state) {
                 ESP_LOGD("custom_switch", "Switch ON-TTTTT");
-                global_mqtt_client->publish("devices/"+id(global_forced_addr)+"/0x0006/0/attributes/0x0000", "ON", 0, true);
+                mqtt::global_mqtt_client->publish("devices/"+id(global_forced_addr)+"/0x0006/0/attributes/0x0000", "ON", 0, true);
             } else {
                 ESP_LOGD("custom_switch", "Switch OFF-TTTT");
-                global_mqtt_client->publish("devices/"+id(global_forced_addr)+"/0x0006/0/attributes/0x0000", "OFF", 0, true);
+                mqtt::global_mqtt_client->publish("devices/"+id(global_forced_addr)+"/0x0006/0/attributes/0x0000", "OFF", 0, true);
             }
         });
         //devices/e89a85eb-452e-4111-9494-619d9ddea73a/0x0006/0/attributes/0x0000
