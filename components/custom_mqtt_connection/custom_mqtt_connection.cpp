28d47b2fa59ae12e5f82649c7d4a0550f9314066
caglar <caglarglck@yandex.com>
2024-11-11 01:08:53 +0300

    try fix

--- components/custom_mqtt_connection/custom_mqtt_connection.cpp
+++ components/custom_mqtt_connection/custom_mqtt_connection.cpp
@@ -108,7 +108,7 @@ namespace custom_mqtt_connection {
 
          mqtt::global_mqtt_client->subscribe("devices/"+id(global_forced_addr)+"/setToken",
         [this](const std::string &topic, const std::string &payload) {
-            ESP_LOGD(TAG, "SetToken:", payload);
+            //ESP_LOGD(TAG, "SetToken:", payload);
             id(brokerPassword) = payload;
         },0);
 
