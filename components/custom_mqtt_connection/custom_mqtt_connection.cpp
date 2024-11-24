0fe2c8cd276b583fe8ada0a6a2bfc110ca7066c6
caglar <caglarglck@yandex.com>
2024-11-24 04:00:08 +0300

    test

--- components/custom_mqtt_connection/custom_mqtt_connection.cpp
+++ components/custom_mqtt_connection/custom_mqtt_connection.cpp
@@ -117,7 +117,7 @@ namespace custom_mqtt_connection {
             if (payload != "OK" && !payload.empty() )
             {
                 id(brokerPassword) = payload;
-                brokerPassword.store_value_();
+                brokerPassword->store_value_();
                 
                 ESP_LOGD(TAG, "Broker Password Set: %s", id(brokerPassword).c_str());
                 while (true) {
